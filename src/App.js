c88b0f978cee6b2ea639a768c776fd4538034fc3
barak <barakreutlevy@gmail.com>
2020-01-26 10:18:01 +0200

    added auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase";
 import "./App.css";
 import NewDog from "./components/newDog/newDog";
 import DogGallery from "./components/viewDogs/dogsGallery";
@@ -6,24 +10,51 @@ import Quiz from "./components/Quiz/quiz";
 import MainPage from "./components/main/mainPage";
 import Login from "./components/login/login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={MainPage} />
-          <Route path="/Quiz" exact component={Quiz} />
-          <Route path="/NewDog" exact component={NewDog} />
-          <Route path="/AllDogs" exact component={DogGallery} />
-          <Route path="/Login" exact component={Login} />
-        </Switch>
-      </Router>
 
-      {/* <NewDog /> */}
-      {/* <DogGallery /> */}
-      {/* <QUIZ /> */}
-    </div>
-  );
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class App extends Component {
+  render() {
+    const { user, signOut, signInWithGoogle } = this.props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {user ? (
+            <div>
+              <Router>
+                <Switch>
+                  <Route path="/" exact component={MainPage} />
+                  <Route path="/Quiz" exact component={Quiz} />
+                  <Route path="/NewDog" exact component={NewDog} />
+                  <Route path="/AllDogs" exact component={DogGallery} />
+                  <Route path="/Login" exact component={Login} />
+                </Switch>
+              </Router>
+
+              {/* <NewDog /> */}
+              {/* <DogGallery /> */}
+              {/* <QUIZ /> */}
+            </div>
+          ) : (
+            <div>
+              <p>Please sign in.</p>
+              <button onClick={signInWithGoogle}>Sign in with Google</button>
+            </div>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App);
--- src/components/viewDogs/dogCard.js
+++ src/components/viewDogs/dogCard.js
@@ -33,9 +33,7 @@ class DogCard extends React.Component {
           />
           <MDBCardBody>
             <MDBCardTitle>{this.props.name}</MDBCardTitle>
-            <mdb-badge pill color="indigo">
-              Indigo
-            </mdb-badge>
+
             <MDBCardText>{this.props.description}</MDBCardText>
             <MDBRow className="d-flex justify-content-center">
               <MDBBtn id="card-btn" size="sm">
